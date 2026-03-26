f053f86cc570d1e9b5de7fdf13c83ea7d5ef477d
Jithu Gopal <gopal.jithu3@gmail.com>
2026-03-26 11:08:22 +0530

    Updated restaurant app and added templates

--- Resturant_Project/Base_App/admin.py
+++ Resturant_Project/Base_App/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from Base_App.models import *
+# Register your models here.
+
+admin.site.register(ItemList)
+admin.site.register(Items)
+admin.site.register(AboutUs)
+admin.site.register(Feedback)
+admin.site.register(BookTable)
--- Resturant_Project/Resturant_Project/urls.py
+++ Resturant_Project/Resturant_Project/urls.py
@@ -1,24 +1,23 @@
-"""
-URL configuration for Resturant_Project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from Base_App.views import *
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin_pannel'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('signup/', SignupView, name='signup'),
+    path('logout/', LogoutView, name='logout'),
+    path('', HomeView, name='Home'),
+    path('book_table/', BookTableView, name='Book_Table'),
+    path('menu/', MenuView, name='Menu'),
+    path('about/', AboutView, name='About'),
+    path('feedback/', FeedbackView, name='Feedback_Form'),
+    path('add-to-cart/', add_to_cart, name='add_to_cart'),
+    path('get-cart-items/', get_cart_items, name='get_cart_items'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
